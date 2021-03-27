f084cfab79edb739701b52b304ddedf1a9d39fbb
Harshiv Patel <pharshiv10@gmail.com>
2021-03-28 03:18:15 +0530

    File Upload

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -30,60 +30,62 @@ function DropZone(props) {
     let tempUrlArray = [];
     let tempErrorArray = [];
     files.map((file, index) => {
-      console.log("File Name", file.name);
-      const storageRef = storage.ref();
-      const fileRef = storageRef.child(file.name);
-      const uploadTask = fileRef.put(file);
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          // Observe state change events such as progress, pause, and resume
-          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-          var progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
-          switch (snapshot.state) {
-            case firebase.storage.TaskState.PAUSED: // or 'paused'
-              console.log("Upload is paused");
-              break;
-            case firebase.storage.TaskState.RUNNING: // or 'running'
-              console.log("Upload is running");
-              break;
-          }
-        },
-        (error) => {
-          // Handle unsuccessful uploads
-          console.log(error);
-          tempErrorArray.push(error);
-        },
-        () => {
-          // Handle successful uploads on complete
-          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-            console.log("File available at", downloadURL);
-            tempUrlArray.push(downloadURL);
-          });
-        }
-      );
-      storageRef.put(file).on(
-        "state_changed",
-        (snap) => {
-          let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
-          setProgress(percentage);
-        },
-        (err) => {
-          let tempErrorArray = errorArray;
-          tempErrorArray.push(err);
-          setErrorArray(tempErrorArray);
-        },
-        async () => {
-          const url = await storageRef.getDownloadURL();
-          let tempUrlArray = urlArray;
-          tempUrlArray.push(url);
-          setUrlArray(tempUrlArray);
-        }
-      );
-    });
+		console.log('File Name', file.name);
+		const storageRef = storage.ref();
+		const fileRef = storageRef.child(file.name);
+		// const uploadTask = fileRef.put(file);
+		fileRef.put(file).then(snapshot => {
+			console.log('Uploaded a blob or file!');
+		});
+		// uploadTask.on(
+		// 	'state_changed',
+		// 	snapshot => {
+		// 		// Observe state change events such as progress, pause, and resume
+		// 		// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+		// 		var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+		// 		console.log('Upload is ' + progress + '% done');
+		// 		switch (snapshot.state) {
+		// 			case firebase.storage.TaskState.PAUSED: // or 'paused'
+		// 				console.log('Upload is paused');
+		// 				break;
+		// 			case firebase.storage.TaskState.RUNNING: // or 'running'
+		// 				console.log('Upload is running');
+		// 				break;
+		// 		}
+		// 	},
+		// 	error => {
+		// 		// Handle unsuccessful uploads
+		// 		console.log(error);
+		// 		tempErrorArray.push(error);
+		// 	},
+		// 	() => {
+		// 		// Handle successful uploads on complete
+		// 		// For instance, get the download URL: https://firebasestorage.googleapis.com/...
+		// 		uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
+		// 			console.log('File available at', downloadURL);
+		// 			tempUrlArray.push(downloadURL);
+		// 		});
+		// 	}
+		// );
+		// storageRef.put(file).on(
+		//   "state_changed",
+		//   (snap) => {
+		//     let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+		//     setProgress(percentage);
+		//   },
+		//   (err) => {
+		//     let tempErrorArray = errorArray;
+		//     tempErrorArray.push(err);
+		//     setErrorArray(tempErrorArray);
+		//   },
+		//   async () => {
+		//     const url = await storageRef.getDownloadURL();
+		//     let tempUrlArray = urlArray;
+		//     tempUrlArray.push(url);
+		//     setUrlArray(tempUrlArray);
+		//   }
+		// );
+	});
   }, [files, errorArray, urlArray]);
 
   useEffect(() => {
--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -11,15 +11,15 @@ import firebase from 'firebase'
 //     measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 // }
 const firebaseConfig = {
-    apiKey: "AIzaSyCCvpyNNukYRpQoFpGiDBhbq2wh6PqVVkE",
-	authDomain: "myspace-ec3c9.firebaseapp.com",
+	apiKey: 'AIzaSyCCvpyNNukYRpQoFpGiDBhbq2wh6PqVVkE',
+	authDomain: 'myspace-ec3c9.firebaseapp.com',
 	databaseURL: 'https://myspace-ec3c9-default-rtdb.firebaseio.com',
-	projectId: "myspace-ec3c9",
-	storageBucket: "myspace-ec3c9.firebaseapp.com",
-	messagingSenderId: "767975049636",
-	appId: "1:767975049636:web:d09fd032eabce318e01931",
-	measurementId: "G-FQ60HV8HPS"
-}
+	projectId: 'myspace-ec3c9',
+	storageBucket: 'myspace-ec3c9.appspot.com',
+	messagingSenderId: '767975049636',
+	appId: '1:767975049636:web:d09fd032eabce318e01931',
+	measurementId: 'G-FQ60HV8HPS',
+};
 firebase.initializeApp(firebaseConfig)
 firebase.analytics();
 const database = firebase.database();
